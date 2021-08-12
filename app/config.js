5a8de1956341d8e01e4585acd8a96550a3564112
Dale Moore <dale@contentdevelopment.co.uk>
2021-08-12 11:01:50 +0100

    Renamed the service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Content Design 101',
 
   // Default port that prototype runs on
   port: '3000',
